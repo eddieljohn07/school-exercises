3a94a8fb3e3d22582b3acda7b8e52ccb2d76c751
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-02-28 03:21:45 +0800

    added conditions for roots

--- c++/quadratic-equation-roots/main.cpp
+++ c++/quadratic-equation-roots/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -9,11 +11,22 @@ int main()
     float x1, x2, x;
     int a, b, c;
     cout << "Enter the value of coefficeint x^2 (a): ";
-    cin >> b;
+    cin >> a;
     cout << "Enter the value of coefficeint x (b): ";
     cin >> b;
     cout << "Enter the value of coefficeint (c): ";
     cin >> c;
     float discriminant = b * b - 4 * a * c;
+    if (discriminant > 0){
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+        cout << "The roots are: " << fixed << setprecision(2) << x1 << "and " << fixed << setprecision(2) << x2;
+        //cout("The roots are: " + to_string(x1) + "and" + to_string(x2));
+    } else if (discriminant == 0){
+        x = -b / (2 *a);
+        cout << "The root is: " << x;
+    } else{
+        cout << "There is no real root.";
+    }
     return 0;
 }
