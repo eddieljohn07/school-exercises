51a61cc176800f8fdc7ae0cba83776beb0c1aaf7
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-02-28 03:01:33 +0800

    initialized discriminant and removed at first initialize

--- c++/quadratic-equation-roots/main.cpp
+++ c++/quadratic-equation-roots/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Get The Roots of a Quadratic Equation!" << endl;
-    float discriminant, x1, x2, x;
+    float x1, x2, x;
     int a, b, c;
     cout << "Enter the value of coefficeint x^2 (a): ";
     cin >> b;
@@ -14,5 +14,6 @@ int main()
     cin >> b;
     cout << "Enter the value of coefficeint (c): ";
     cin >> c;
+    float discriminant = b * b - 4 * a * c;
     return 0;
 }
