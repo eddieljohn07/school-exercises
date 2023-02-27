5e56b08539e4fba58654a8ef65727768895444b0
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-02-28 05:08:35 +0800

    add while loop to meet conditions

--- c++/pattern/main.cpp
+++ c++/pattern/main.cpp
@@ -4,5 +4,15 @@ using namespace std;
 int main()
 {
     int x = 1, y;
+     while (x <= 5) {
+        y = 1;
+        while (y <= x) {
+            cout << "*";
+            y++;
+        }
+        cout << endl;
+        x++;
+    }
     return 0;
+
 }
