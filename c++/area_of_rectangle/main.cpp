41f0cdced576d1785813b482b014d39b4c8e6678
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-02-27 21:23:01 +0800

    changed data type from double to float

--- c++/area_of_rectangle/main.cpp
+++ c++/area_of_rectangle/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    double length;
-    double width;
+    float length, width;
     cout << "Please enter the length of the rectangle: ";
     cin >> length;
     cout << "Please enter the width of the rectangle: ";
     cin >> width;
+    float area = length * width;
+    cout << "The area of the reactangle is " + to_string(area) + "!";
+
     return 0;
 }
