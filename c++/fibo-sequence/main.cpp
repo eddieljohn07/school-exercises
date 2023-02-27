623836df48601955d8535ffcbecbd4a1decfc222
ej <100287870+eddieljohn07@users.noreply.github.com>
2023-02-28 04:40:58 +0800

    add while loop

--- c++/fibo-sequence/main.cpp
+++ c++/fibo-sequence/main.cpp
@@ -6,5 +6,14 @@ int main()
 {
     int num = 20;
     int a = 0, b = 1;
+    cout << a << ", ";
+    cout << b << ", ";
+    while (num > 2) {
+        int c = a + b;
+        cout << c << ", ";
+        a = b;
+        b = c;
+        num--;
+    }
     return 0;
 }
