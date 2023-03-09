f1a66a54c03a9b3d387f00cf09ed9d46a6c0b9cd
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-03-09 22:43:51 +0800

    add  print to output conversion

--- java/Fahrenheit-to-Celcius/main.java
+++ java/Fahrenheit-to-Celcius/main.java
@@ -3,4 +3,7 @@ public class Main {
     public static void main(String[] args) {
       double tempCelcius = 41.59d;
       double tempFahrenheit = tempCelcius * 1.8 + (32);
+      System.out.println((tempCelcius + (" degree celcius is ") + tempFahrenheit + (" degree fahrenheit")));
+    }
+} 
       
